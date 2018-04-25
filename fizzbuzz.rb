0155454464c280abe823fee4a29f035c4f93cb01
Pablo De La Cruz <pdelac@ramapo.edu>
2018-04-25 15:26:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@ def fizzbuzz (x)
   if (x % 3 == 0 && x % 5 == 0)
     "FizzBuzz"
   
-  elseif (x % 5 == 0)
+  if (x % 5 == 0)
     "Buzz"
   
-  elseif (x % 3 == 0)
+  if (x % 3 == 0)
     "Fizz"
   end
 end
